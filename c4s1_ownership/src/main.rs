2e1ef66e402f91d1c9b0255f7f0c954afdf095ac
忈桀 <ren.ie@outlook.com>
2023-07-27 00:05:29 +0800

    update

--- c4s1_ownership/src/main.rs
+++ c4s1_ownership/src/main.rs
@@ -24,6 +24,18 @@ fn main() {
     // Listing 4-5: Returning ownership of parameters
     println!("\n>> Listing 4-5: Returning ownership of parameters");
     return_ownership();
+
+    let ret_s = gives_ownership();
+    println!("return val of gives_ownership: {}", ret_s);
+
+    let arg_s = String::from("hello");
+    let ret_s2 = takes_and_gives_back(arg_s);
+    println!("return val of takes_and_gives_back: {}", ret_s2);
+
+    println!("\n>> return multiple values using a tuple");
+    let s3 = String::from("hello");
+    let (s4, len) = calculate_length(s3);
+    println!("Thr length of \"{}\" is {}", s4, len);
 }
 
 fn takes_ownership(some_string: String) {
@@ -43,4 +55,13 @@ fn return_ownership() {
 fn calculate_length(s :String) -> (String, usize) {
     let length = s.len();
     (s, length)
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_back(string: String) -> String {
+    string
 }
